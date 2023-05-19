bd12fa4eb9e53b2c5f91c9b851004607e4f3ee0f
Yaakov Margalit <yaakovmargalit@gmail.com>
2023-05-19 08:42:55 +0300

    אגדרכד

--- index.js
+++ index.js
@@ -1,68 +1,77 @@
 const fs = require('fs');
+const crypto = require('crypto');
 
 
 
-toJSON=()=>{
-    const text = fs.readFileSync('./file.strings', 'utf-8');
+toJSON = () => {
+  const text = fs.readFileSync('./file.strings', 'utf-8');
 
 
-const result = {};
+  const result = {};
 
-// פיצול הטקסט לשורות
-const lines = text.split('\n');
+  // פיצול הטקסט לשורות
+  const lines = text.split('\n');
 
-// עבור כל שורה בטקסט
-for (const line of lines) {
-  // אם השורה אינה ריקה
-  if (line.trim() !== '') {
-    // פיצול השורה למפתח וערך
-    const [key, value] = line.split('=');
-    const fixedValue = value.replace(/[";]/g, '');
-        const fixedKey = key.replace(/"/g, '');
-    // הוספת המפתח והערך לאובייקט התוצאה
-    result[fixedKey.trim()] = fixedValue.trim();
+  // עבור כל שורה בטקסט
+  for (const line of lines) {
+    // אם השורה אינה ריקה
+    if (line.trim() !== '') {
+      // פיצול השורה למפתח וערך
+      const [key, value] = line.split('=');
+      const fixedValue = value.replace(/[";]/g, '');
+      const fixedKey = key.replace(/"/g, '');
+      // הוספת המפתח והערך לאובייקט התוצאה
+      result[fixedKey.trim()] = fixedValue.trim();
+    }
   }
+
+  // המרת המערך למחרוזת JSON
+  const jsonResult = JSON.stringify(result, null, 2);
+
+  // כתיבת התוצאה לקובץ
+  fs.writeFileSync('./output.json', jsonResult, 'utf-8');
+
+  console.log('המרה הושלמה בהצלחה!');
 }
 
-// המרת המערך למחרוזת JSON
-const jsonResult = JSON.stringify(result, null, 2);
-
-// כתיבת התוצאה לקובץ
-fs.writeFileSync('./output.json', jsonResult, 'utf-8');
-
-console.log('המרה הושלמה בהצלחה!');
-}
 
 
+fromJSON = () => {
+  const jsonString = fs.readFileSync('./output.json', 'utf-8');
 
-fromJSON=()=>{
-    const jsonString = fs.readFileSync('./output.json', 'utf-8');
+  // המרת המחרוזת המכילה את ה-JSON לאובייקט
+  const jsonData = JSON.parse(jsonString);
 
-// המרת המחרוזת המכילה את ה-JSON לאובייקט
-const jsonData = JSON.parse(jsonString);
+  // מערך שבו נאחסן את התוצאה
+  const result = [];
 
-// מערך שבו נאחסן את התוצאה
-const result = [];
+  // עבור כל מאפיין באובייקט ה-JSON
+  for (const key in jsonData) {
+    if (jsonData.hasOwnProperty(key)) {
+      // יצירת שורת טקסט עם המאפיין והערך המתאים
+      const line = `"${key}" = "${jsonData[key]}";`;
 
-// עבור כל מאפיין באובייקט ה-JSON
-for (const key in jsonData) {
-  if (jsonData.hasOwnProperty(key)) {
-    // יצירת שורת טקסט עם המאפיין והערך המתאים
-    const line = `"${key}" = "${jsonData[key]}";`;
-    
-    // הוספת השורה למערך התוצאה
-    result.push(line);
+      // הוספת השורה למערך התוצאה
+      result.push(line);
+    }
   }
+
+  // המרת מערך התוצאה למחרוזת והוספת יישורים
+  const outputString = result.map(line => '  ' + line).join('\n');
+// חישוב ערך ה-hash לשם הקובץ
+const hash = crypto.createHash('md5').update(outputString).digest('hex');
+
+// שם הקובץ החדש עם ה-hash
+const outputFileName = `baba.${hash}.strings`;
+  // כתיבת התוצאה לקובץ
+  fs.writeFileSync(outputFileName, outputString, 'utf-8');
+
+  console.log('המרה הושלמה בהצלחה!');
 }
 
-// המרת מערך התוצאה למחרוזת והוספת יישורים
-const outputString = result.map(line => '  ' + line).join('\n');
 
-// כתיבת התוצאה לקובץ
-fs.writeFileSync('tata.strings', outputString, 'utf-8');
 
-console.log('המרה הושלמה בהצלחה!');
-}
-
-toJSON()
-fromJSON()
+setInterval(() => {
+  toJSON()
+  fromJSON()
+}, 2000)
